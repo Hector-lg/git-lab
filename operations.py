b7c001ed434e35eb101859e26314a1fa58c1e191
Luis Velediaz <a01029829@tec.mx>
2025-02-17 15:22:25 -0600

    Added code to operations.py

--- operations.py
+++ operations.py
@@ -1,8 +1,10 @@
 a = int(input(“Give me the first number:”))
 b = int(input(“Give me the second number:”))
-c = int("input("Pon otro numero"))
+c = int(input("Pon otro numero"))
+
 # Display the addition
+print("The result of the addition is: ", a+b)
 
 # Display the subtraction
+print("The result of the substraction is: ", a-b)
 
-# Display multiplication
